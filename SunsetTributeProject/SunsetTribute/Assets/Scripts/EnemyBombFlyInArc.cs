ea51b1ffce4db933b481c4d7aedd982b43c6d56c
Tiryth <felipemilkewicz@gmail.com>
2018-11-27 19:00:33 -0200

    Inimigo "3"
    Coloquei um script que faz o inimigo soltar uma bomba em vez de dar tiro, da pra testar no lvl prog

---
+++ SunsetTributeProject/SunsetTribute/Assets/Scripts/EnemyBombFlyInArc.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBombFlyInArc : MonoBehaviour {
+
+
+    //Todas cordenadas é o "EnemyBomb" Script que passa
+    public Vector3 startPos; // onde começa
+    public Vector3 endPos; //onde cai
+    public float trajectoryHeight = 5.0f; //o quanto o tiro sobe
+    public float velocidadeTiro   = 0.5f;
+
+
+    private float  cTime; //tempo somado para ter trajetoria
+
+    public Transform explosaoFogo;
+//var dano : Transform;
+
+
+    void Start()
+    {
+        cTime = 0f; //começa sempre como zero
+    }
+
+    void Update()
+    {
+
+        // calculate current time within our lerping time range
+        cTime += Time.deltaTime * velocidadeTiro;
+
+        // calculate straight-line lerp position:
+        Vector3 currentPos = Vector3.Lerp(startPos, endPos, cTime);
+
+        // add a value to Y, using Sine to give a curved trajectory in the Y direction
+        currentPos.y += trajectoryHeight * Mathf.Sin(Mathf.Clamp01(cTime) * Mathf.PI);
+
+        // finally assign the computed position to our gameObject:
+        transform.position = currentPos;
+        if (currentPos == endPos)
+        {
+            explode();
+        }
+
+    }
+
+    void explode()
+    {
+        var explosao1 = Instantiate(explosaoFogo, transform.position, transform.rotation);
+        //var dano1 = Instantiate(dano, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+
+    /*function OnTriggerEnter(other : Collider)
+    {
+        if (other.transform.tag == "caixa")
+        {
+            Destroy(gameObject);
+        }
+    }*/
+}
---
+++ SunsetTributeProject/SunsetTribute/Assets/Scripts/EnemyBombMarkDestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBombMarkDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (gameObject != null)
+        {
+            Destroy(gameObject, 2.5f);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Bomb"))
+        {
+            Destroy(gameObject);
+            print("entrou");
+        }
+    }
+       
+}
